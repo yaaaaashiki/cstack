460468d7d47858b7e38640622c64d947858b3520
yaaaaashiki <yaaaaakishi@gmail.com>
2017-09-26 22:26:18 +0900

    Remove unnecesarry user model's struct

--- domain/model/user.go
+++ domain/model/user.go
@@ -1,15 +1,6 @@
 package model
 
 type User struct {
-	ID        uint
-	Name      string
-	Email     string
-	Salt      string
-	Salted    string
-	IconImage string
-}
-
-type UserData struct {
 	ID        uint   `json:"id" gorm:"column:id"`
 	Name      string `json:"name" gorm:"column:name"`
 	Email     string `json:"email" gorm:"column:email"`
