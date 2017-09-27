221a8d27c352151ab844c9bb7724cbb7cfdcc336
yaaaaashiki <yaaaaakishi@gmail.com>
2017-09-27 19:39:31 +0900

    Remove unsed function

--- helper/sessionutil.go
+++ helper/sessionutil.go
@@ -1,27 +1,9 @@
 package helper
 
 import (
-	"errors"
-
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
-
-func ClearSession(g *gin.Context) {
-	session := sessions.Default(g)
-	session.Clear()
-	session.Save()
-}
-
-func GetSession(g *gin.Context) (email string, userID uint, error error) {
-	session := sessions.Default(g)
-	emailValue := session.Get("email")
-	userIDValue := session.Get("userID")
-	if emailValue == nil || userIDValue == nil {
-		return "", 0, errors.New("It has not been authenticated")
-	}
-	return emailValue.(string), userIDValue.(uint), nil
-}
 
 func SaveSession(g *gin.Context, email string, userID uint) error {
 	session := sessions.Default(g)
